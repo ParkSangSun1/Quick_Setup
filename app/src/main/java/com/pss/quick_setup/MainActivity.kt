53e132c2a789381e6b22966063fdfec39b476a6e
ParkSangSun <appleasz1028102@gmail.com>
2021-12-13 17:04:29 +0900

    init

---
+++ app/src/main/java/com/pss/quick_setup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pss.quick_setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
